e58b4c5467a44dcba608eea124ffa284b79bafe3
echris10sen <chr15016@byui.edu>
2023-02-10 10:33:27 -0700

    added things to chamber

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,23 @@
+document.querySelector("#menu").addEventListener('click', ()=>{
+    document.querySelector("#open").classList.toggle('active_button');
+    document.querySelector("#closed").classList.toggle('active_button');
+    document.querySelector("#navbar").classList.toggle('active_button');
+});
+
+const today = new Date();
+
+// Display current year
+document.querySelector('#currentyear').textContent = today.getFullYear();
+
+// Display
+document.querySelector('#currentdate').textContent = today.toLocaleString("en-us", {
+    weekday: "long",
+    day: "2-digit",
+    month: 'long',
+    year: 'numeric'
+
+});
+// This sets last modified date
+document.querySelector('#lastmodified').textContent = document.lastModified;
+
+// adding comment to update
